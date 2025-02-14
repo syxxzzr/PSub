3f0ccc67a15d83ac85b73f757884347c6e60eb00
syxxzzr <syxxzzr@163.com>
2025-02-14 17:44:03 +0800

    Update

--- ass/__init__.py
+++ ass/__init__.py
@@ -6,23 +6,29 @@ import re
 
 
 def parse(
-            ass: Union[str, pathlib.Path, _io.TextIOWrapper],
-            encoding: str = 'utf-8-sig'
-    ):
-        if isinstance(ass, str):
-            if os.path.exists(ass):
-                ass = pathlib.Path(ass)
-            else:
-                ass_lines = re.split(r'\r?\n', ass)
-        if isinstance(ass, pathlib.Path):
-            ass = open(ass, 'r', encoding=encoding)
-        if isinstance(ass, _io.TextIOWrapper):
-            ass_lines = ass.readlines()
+        ass: Union[str, pathlib.Path, _io.TextIOWrapper],
+        encoding: str = 'utf-8-sig'
+):
+    if isinstance(ass, str):
+        if os.path.exists(ass):
+            ass = pathlib.Path(ass)
+        else:
+            ass_lines = re.split(r'\r?\n', ass)
+    if isinstance(ass, pathlib.Path):
+        ass = open(ass, 'r', encoding=encoding)
+    if isinstance(ass, _io.TextIOWrapper):
+        ass_lines = ass.readlines()
+    ass_lines = iter(ass_lines)
 
-        section_state = 0
-        for line in ass_lines:
-            if re.match(r'^;|!', line):
-                continue
+    section_state = 0
+    for line in ass_lines:
+        if re.match(r'^;|!', line):
+            continue
+        section_name = re.match(r'^\[(.*)]$', line)
+        if section_name:
+            section_name = section_name.group()
+            pass
+
 
 # class ASS:
 #     def __init__(
@@ -32,5 +38,5 @@ def parse(
 #     ):
 #         pass
 
-
-
+if __name__ == '__main__':
+    ass_parser = parse(r'./test.ass')
